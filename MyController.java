3b71009f37d6d08932186bcd85676cfe1080746a
Dixitkolupula <74012136+Dixitkolupula@users.noreply.github.com>
2020-12-07 14:49:21 +0530

    Add files via upload

---
+++ MyController.java
@@ -0,0 +1,17 @@
+package com.ecomm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+	@Controller
+	public class MyController
+	{
+		@RequestMapping("/login")
+		public String showLoginPage()
+		{
+			System.out.println("===Iam in My Controller-Login Page -=======");
+			return "Login";
+		}
+	}
+
+  
---
+++ index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html"%>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+<center>
+    <a href="login">Login Page</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="showCategory">Show Category</a>
+    </center>
+</body>
+</html>
